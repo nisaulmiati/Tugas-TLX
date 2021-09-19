181816757f125d90228e68c321cfd7197dc7f850
nisaulmiati <90362570+nisaulmiati@users.noreply.github.com>
2021-09-19 16:03:25 +0700

    Add files via upload

---
+++ AtambahB.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AtambahB {
+    public static void main(String[] args) {
+
+        int A, B, total;
+
+        Scanner bebek = new Scanner(System.in);
+        A = bebek.nextInt();
+        B = bebek.nextInt();
+
+        total = A+B;
+
+        System.out.println(total);
+
+        
+    }
+    
+}
